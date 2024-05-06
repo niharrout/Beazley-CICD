61a3fed1f6ea318cfb9887a9c2e4f75cb4756311
Sam <pandasamaptika96@gmail.com>
2024-05-06 16:03:54 +0530

    Delete PERFDIP.sql

--- SGSSANDBOX/DBO/TABLE/PERFDIP.sql
+++
@@ -1,5 +0,0 @@
-create or replace TABLE "DBO".PERFDIP (
-	DEPARTMENT_ID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 order,
-	DEPARTMENT_NAME VARCHAR(50),
-	primary key (DEPARTMENT_ID)
-);
