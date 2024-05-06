1d534067127af3aad7f228907b47605da38bfa91
Sam <pandasamaptika96@gmail.com>
2024-05-06 16:57:18 +0530

    Delete ORDERS09.sql

--- SGSSANDBOX_UserID/DBO/TABLE/ORDERS09.sql
+++
@@ -1,8 +0,0 @@
-create or replace TABLE "DBO".ORDERS09 (
-	ORDER_ID NUMBER(38,0) NOT NULL autoincrement start 1 increment 1 order,
-	CUSTOMER_ID NUMBER(38,0),
-	ORDER_DATE DATE,
-	TOTAL_AMOUNT NUMBER(10,2),
-	primary key (ORDER_ID),
-	foreign key (CUSTOMER_ID) references SGSSANDBOX.DBO.CUSTOMERS09(CUSTOMER_ID)
-);
