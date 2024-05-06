3030c0ba59385ac548b0ecb107a2965e11f6cc5d
Sam <pandasamaptika96@gmail.com>
2024-05-06 12:24:57 +0530

    test

---
+++ actions/4DCHECKS/src/action.js
@@ -0,0 +1,98 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fetch = require("node-fetch");
+
+async function run() {
+  let USER_NAME = core.getInput("USER_NAME");
+  let P_W = core.getInput("P_W");
+  let SourceSystem = core.getInput("SOURCE_SYS");
+  let TargetSystem = core.getInput("TARGET_SYS");
+  let schemaName = core.getInput("SCHEMA");
+
+  // HEAD_REF: ${{github.head_ref}}
+  // BASE_REF: ${{github.base_ref}}
+  let head_ref = core.getInput("HEAD_REF");
+  let base_ref = core.getInput("BASE_REF");
+
+  console.log({ head_ref, base_ref, SourceSystem, TargetSystem, schemaName });
+
+  let body_0 = {
+    username: USER_NAME,
+    password: P_W,
+  };
+
+  let body_1 = {
+    UserName: "<UserName>",
+    RepoType: "GITHUB",
+    RepoName: "GITHUB-<RepoName>",
+    ConnectionName: "<ConnectionName>",
+    SourceSystemName: "<SourceSystemName>",
+    TargetSystemName: "<TargetSystemName>",
+    SystemType: "SNOWFLAKE",
+    SchemaName: ["<SchemaName>"],
+    Path: "DATABASE",
+    HeadBranch: "develop",
+    BaseBranch: "master",
+    ScriptGenerationRules : [""]
+  };
+  const TokenFetchResponse = await fetch(
+    `<Application_URL>/api/v1/user-auth/login-user`,
+    {
+      method: "post",
+      body: JSON.stringify(body_0),
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  const Tokendata = await TokenFetchResponse.json();
+  const Token = Tokendata.token;
+
+  // const response = await fetch(
+  //   "http://40.78.156.172/api/v1/4dalert/database-data-change-monitor?database=decisionsigma",
+  //   {
+  //     method: "get",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       cookie: `4dalert-user-token=${Token}`,
+  //     },
+  //   }
+  // );
+
+  const deployScriptResp = await fetch(
+    `<Application_URL>/api/v1/4d/ci/cd/generate-deployment-scripts`,
+    {
+      method: "post",
+      body: JSON.stringify(body_1),
+      headers: {
+        "Content-Type": "application/json",
+        cookie: `4dalert-user-token=${Token}`,
+      },
+    }
+  );
+
+  console.log("deployScriptResp : ", deployScriptResp);
+
+  // const ResonseData = await response.text();
+  // console.log("ResonseData : ", ResonseData);
+
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  // await octokit.rest.issues.createComment({
+  //   ...context.owner,
+  //   ...context.repo,
+  //   issue_number: pull_request.number,
+  //   body: `Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n${ResonseData}`,
+  // });
+
+  await octokit.rest.issues.createComment({
+    ...context.owner,
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: `Thank you for submitting a pull request! We will try to review this as soon as we can.`,
+  });
+}
+
+run();
---
+++ actions/snowflake_deploy/src/action.js
@@ -0,0 +1,91 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fetch = require("node-fetch");
+
+async function run() {
+  let sleep = function (ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  let commentBody = "";
+  const USER_NAME = core.getInput("USER_NAME");
+  const P_W = core.getInput("P_W");
+  let SourceSystem = core.getInput("SOURCE_SYS");
+  let TargetSystem = core.getInput("TARGET_SYS");
+
+  // HEAD_REF: ${{github.head_ref}}
+  // BASE_REF: ${{github.base_ref}}
+  let head_ref = core.getInput("HEAD_REF");
+  let base_ref = core.getInput("BASE_REF");
+  let merge_status = core.getInput("MERGE_STATUS");
+  let schemaName = core.getInput("SCHEMA");
+
+  console.log({ head_ref, base_ref, schemaName, merge_status });
+
+  if (merge_status == "true") {
+    await sleep(20000);
+
+    let body_0 = {
+      username: USER_NAME,
+      password: P_W,
+    };
+    let body_1 = {
+      UserName: "<UserName>",
+      RepoType: "GITHUB",
+      RepoName: "GITHUB-<RepoName>",
+      ConnectionName: "<ConnectionName>",
+      SourceSystemName: "<SourceSystemName>",
+      TargetSystemName: "<TargetSystemName>",
+      SystemType: "SNOWFLAKE",
+      SchemaName: ["<SchemaName>"],
+      HeadBranch: "",
+      BaseBranch: "master",
+      Path: "Migration",
+    };
+
+    const TokenFetchResponse = await fetch(
+      `<Application_URL>/api/v1/user-auth/login-user`,
+      {
+        method: "post",
+        body: JSON.stringify(body_0),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    const Tokendata = await TokenFetchResponse.json();
+    const Token = Tokendata.token;
+    const resp = await fetch(
+      `<Application_URL>/api/v1/4d/ci/cd/execute-system-ddl-query`,
+      {
+        method: "post",
+        body: JSON.stringify(body_1),
+        headers: {
+          "Content-Type": "application/json",
+          cookie: `4dalert-user-token=${Token}`,
+        },
+      }
+    );
+
+    // console.log("deployScriptResp : ", deployScriptResp);
+    console.log({ resp });
+
+    // commentBody = await resp.data.text();
+    commentBody = await resp.data;
+    // console.log("commentBody : ", commentBody);
+  }
+
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  await octokit.rest.issues.createComment({
+    ...context.owner,
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: `Your Merge process completed successfully.`,
+    // body: `Your Merge process completed successfully.\n\n${commentBody}`,
+  });
+}
+
+run();
